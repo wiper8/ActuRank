5d0a6cd22f40b02b6e5b0e770880a49a291b2bc3
William Perron <william.perron.3@ulaval.ca>
2024-06-20 06:43:15 -0400

    statistiques par joueur

--- src/scores.R
+++ src/scores.R
@@ -23,3 +23,48 @@ add_scores <- function(daily_scores, scores, date) {
   res
 }
 
+scores_per_pt_converter <- function(score) {
+  A <- as.numeric(score["score_A"])
+  B <- as.numeric(score["score_B"])
+  res <- as.data.frame(matrix(score, nrow = A + B, ncol = 8, byrow=T, dimnames = list(NULL, names(score))))
+  if(A >= 1) {
+    res[1:A, "win"] <- 1
+    res[1:A, "score_A"] <- 1
+    res[1:A, "score_B"] <- 0
+  }
+  if(B >= 1) {
+    res[(A + 1) : (A + B), "win"] <- 0
+    res[(A + 1) : (A + B), "score_A"] <- 0
+    res[(A + 1) : (A + B), "score_B"] <- 1
+  }
+  res$game_length <- 1
+  res
+}
+
+scores_stats <- function(scores, players) {
+  #games played per players
+  games_played <- sapply(names(players), function(nom) sum(apply(scores[, paste0("joueur_", c("A1", "A2", "B1", "B2"))], 1, function(game) nom %in% game)))
+  
+  list(
+    games_played = games_played[order(games_played, decreasing = T)],
+    
+    #points gained/loss per players
+    pts = sapply(names(sort(games_played, decreasing = T)), function(nom) {
+      keep_A <- apply(scores[, paste0("joueur_", c("A1", "A2"))], 1, function(game) nom %in% game)
+      keep_B <- apply(scores[, paste0("joueur_", c("B1", "B2"))], 1, function(game) nom %in% game)
+      pts_win <- sum(as.numeric(scores[keep_A, "score_A"])) + sum(as.numeric(scores[keep_B, "score_B"]))
+      pts_played <- sum(as.numeric(scores[keep_A | keep_B, "score_A"])) + sum(as.numeric(scores[keep_A | keep_B, "score_B"]))
+      
+      c(pts_win = round(pts_win, 1), pts_played = round(pts_played, 1), ratio = round(pts_win / pts_played, 3))
+    }
+    )
+  )
+}
+
+games_matchups <- function(scores, players) {
+  noms <- names(players)
+  combins <- combn(noms, 2)
+  games_1vs1 <- apply(scores, 1, function(x) is.na(x["joueur_A1"]))
+  res <- apply(combins, 2, function(noms) sum(apply(scores[games_1vs1, c("joueur_A2", "joueur_B1"), drop=F], 1, function(x) all(noms %in% x))))
+  data.frame(A=combins[1, ][order(res, decreasing = T)], B=combins[2, ][order(res, decreasing = T)], count=res[order(res, decreasing = T)])
+}
