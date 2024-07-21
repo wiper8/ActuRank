cd34356912f2f29722214d344670b9ea0a2e7345
William Perron <wil.perron@gmail.com>
2024-07-20 22:32:34 -0400

    ne pas interpoler pour le graphique

--- src/plots.R
+++ src/plots.R
@@ -210,23 +210,8 @@ show_detailed_skill <- function(players) {
   )
   
   players2 <- lapply(players, function(distr) {
-    y <- sapply(init_distr()[, "mu"], function(x) sum(distr[, "p"] * dnorm(x, distr[, "mu"], 1.5*mean(diff(distr[, "mu"])))))
-    
-    #pour simplifier davantage
-    mu <- init_distr()[, "mu"]
-    keep <- y > max(y) / 1000
-    id_first <- which(keep)[1]
-    id_last <- tail(which(keep), 1)
-    if(id_first > 1) keep[id_first - 1] <- TRUE
-    if(id_last < length(keep)) keep[id_last - 1] <- TRUE
-    
-    mu <- mu[keep]
-    y <- y[keep]
-    y <- y/sum(y)
-    
-    cbind(mu=mu, p=y)
+    distr[distr[, "p"] > 0, , drop = FALSE]
   })
-  
   
   graph_data2 <- do.call(rbind, mapply(function(x, n) data.frame(x, "player"=n), players2, names(players2), SIMPLIFY = FALSE))
   
@@ -254,7 +239,6 @@ show_detailed_skill_per_player <- function(players) {
   players2 <- lapply(players, function(distr) {
     distr[distr[, "p"] > 0, , drop = FALSE]
   })
-  
   
   graph_data2 <- do.call(rbind, mapply(function(x, n) data.frame(x, "player"=n), players2, names(players2), SIMPLIFY = FALSE))
   
